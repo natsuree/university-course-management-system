978a39e7bc05272556b47cf72746c84070df2eac
Geo Clark <geoclarkcalasag26@gmail.com>
2026-05-01 02:53:48 +0800

    restart

--- routes/api.php
+++
@@ -1,7 +0,0 @@
-Route::post('/payment/webhook', function(Request $request) {
-    Log::info('Payment Webhook Received:', $request->all());
-
-    return response()->json([
-        'message' => 'Webhook received successfully'
-    ]);
-});
